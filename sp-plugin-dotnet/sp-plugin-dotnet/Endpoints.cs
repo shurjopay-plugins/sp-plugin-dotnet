93cc98c534d8335108bb103ef5b83141bfcda3e0
Mahabubul Hasan <73310789+MahabubulHasanAkash@users.noreply.github.com>
2022-11-30 13:23:05 +0600

    Change property name Verify_Order to Verify_Payment & remove unused namespaces

--- sp-plugin-dotnet/sp-plugin-dotnet/Endpoints.cs
+++ sp-plugin-dotnet/sp-plugin-dotnet/Endpoints.cs
@@ -1,16 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace sp_plugin_dotnet
+﻿namespace sp_plugin_dotnet
 {
     public static class Endpoints
     {
         public const string TOKEN = "get_token";
         public const string MAKE_PAYMENT = "secret-pay";
-        public const string VERIFY_ORDER = "verification";
+        public const string VERIFY_PAYMENT = "verification";
         public const string PAYMENT_STATUS = "payment-status";
     }
 
