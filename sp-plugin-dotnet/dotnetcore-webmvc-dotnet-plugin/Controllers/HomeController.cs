6d1aaaf84476e8d9dc82bdc05766e37e0bc6322b
Mahabubul Hasan <73310789+MahabubulHasanAkash@users.noreply.github.com>
2022-12-04 16:14:54 +0600

    Add Shurjopay Link to Home

--- sp-plugin-dotnet/dotnetcore-webmvc-dotnet-plugin/Controllers/HomeController.cs
+++ sp-plugin-dotnet/dotnetcore-webmvc-dotnet-plugin/Controllers/HomeController.cs
@@ -17,32 +17,6 @@ namespace dotnetcore_webmvc_dotnet_plugin.Controllers
 
         public IActionResult Index()
         {
-            Shurjopay shurjopay= new Shurjopay();
-            PaymentRequest paymentRequest = new PaymentRequest();
-            paymentRequest.Amount = 10;
-            paymentRequest.Prefix = "sp-dotnet";
-            paymentRequest.Currency = "BDT";
-            paymentRequest.OrderId = "aps-5221";
-            paymentRequest.CustomerName = "Mahabubul";
-            paymentRequest.CustomerAddress = "APS";
-            paymentRequest.CustomerCity = "Dhaka";
-            paymentRequest.CustomerPhone = "01311310975";
-            paymentRequest.CustomerPostCode = "1229";
-
-            Task<PaymentDetails?> paymentDetailsTask =  shurjopay.MakePayment(paymentRequest);
-            PaymentDetails? paymentDetails = paymentDetailsTask.Result;
-            ViewBag.paymentDetails = paymentDetails;
-
-            //Console.WriteLine(shurjopay.IsTokenExpired());
-            /*
-            Task<VerifiedPayment?> paymentStatusTask = shurjopay.CheckPayment("spay612b73a935ab1");
-            VerifiedPayment? verifiedPayment = paymentStatusTask.Result;
-            */
-            /*
-            Task<VerifiedPayment?> verifiedPaymentTask = shurjopay.VerifyPayment("spay612b73a935ab1");
-            VerifiedPayment? verifiedPayment = verifiedPaymentTask.Result;
-            */
-
             return View();
         }
 
