2b3ac2024a9ec4773ecaec471ebdf24e939d1627
Mahabubul Hasan <73310789+MahabubulHasanAkash@users.noreply.github.com>
2023-01-04 11:39:36 +0600

    Update PayemntRequest Attributes are no longer nullable

--- sp-plugin-dotnet/sp-plugin-dotnet/Models/PaymentRequest.cs
+++ sp-plugin-dotnet/sp-plugin-dotnet/Models/PaymentRequest.cs
@@ -12,31 +12,31 @@ namespace Shurjopay.Plugin.Models
     {
 
         [JsonPropertyName("prefix")]
-        public string? Prefix { get; set; }
+        public string Prefix { get; set; }
 
         [JsonPropertyName("amount")]
-        public double? Amount { get; set; }
+        public double Amount { get; set; }
 
         [JsonPropertyName("order_id")]
-        public string? OrderId { get; set; }
+        public string OrderId { get; set; }
 
         [JsonPropertyName("currency")]
-        public string? Currency { get; set; }
+        public string Currency { get; set; }
 
         [JsonPropertyName("customer_name")]
-        public string? CustomerName { get; set; }
+        public string CustomerName { get; set; }
 
         [JsonPropertyName("customer_address")]
-        public string? CustomerAddress { get; set; }
+        public string CustomerAddress { get; set; }
 
         [JsonPropertyName("customer_phone")]
-        public string? CustomerPhone { get; set; }
+        public string CustomerPhone { get; set; }
 
         [JsonPropertyName("customer_city")]
-        public string? CustomerCity { get; set; }
+        public string CustomerCity { get; set; }
 
         [JsonPropertyName("customer_post_code")]
-        public string? CustomerPostCode { get; set; }
+        public string CustomerPostCode { get; set; }
         
     }
 }
