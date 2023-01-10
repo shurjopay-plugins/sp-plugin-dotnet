60321cf70e7290cc9edb8f8e98e7fe3cbb8a8c04
Mahabubul Hasan <73310789+MahabubulHasanAkash@users.noreply.github.com>
2023-01-10 14:00:19 +0600

    Delete dotnet core example app

--- sp-plugin-dotnet/dotnetcore-webmvc-dotnet-plugin/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace dotnetcore_webmvc_dotnet_plugin.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
